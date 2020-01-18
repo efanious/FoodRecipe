bcac6ef1a877a5b9924d49e20a6a6fbcd3741d88
Efana Usua <Efana.usua@wemabank.com>
2020-01-18 18:57:49 +0100

    created Constants class

---
+++ app/src/main/java/com/example/foodrecipes/util/Constants.java
@@ -0,0 +1,8 @@
+package com.example.foodrecipes.util;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://www.food2fork.com";
+
+    public static final String API_KEY = "";
+}
